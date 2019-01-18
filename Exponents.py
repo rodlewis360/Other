742839e9139612340d1e4670f0b81e11ce645d99
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2019-01-18 08:44:30 -0800

    Update Exponents.py

--- Exponents.py
+++ Exponents.py
@@ -1,15 +1,19 @@
-list1 = list(range(1, 101))
-list2 = []
-for a in list1:
-    b = a + 1
-    if a ** b == b ** a:
-        list2.append(1)
-    else:
-        list2.append(0)
-b = 0
-for a in list2:
-    b += a
-    c += 1
-average = b / c
-averagepercent = average * 100
-print("This algorithm is correct",averagepercent,"% of the time!")
+def exponents():
+    list1 = list(range(0, 101))
+    list2 = []
+    for a in list1:
+        b = a + 1
+        if a ** b == b ** a:
+            list2.append(1)
+            print("1")
+        else:
+            list2.append(0)
+            print("0")
+    b = 0
+    c = 0
+    for a in list2:
+        b += a
+        c += 1
+    averagevariable = b / c
+    averagepercent = averagevariable * 100
+    print("This algorithm is correct",averagepercent,"% of the time!")
