49eeb895ec3b56539c2d3edb722f4682340b7c9e
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2019-03-14 15:06:32 -0700

    Created base code for calculating e.

--- e.py
+++ e.py
@@ -1,1 +1,12 @@
-
+def e():
+    from time import sleep()
+    print("What set of multiples would you like to have a list of?")
+    exponent = int(input())
+    n = 1
+    print("n   e")
+    while n < 1000000000000000000000000000000000000000:
+      e = (1 + 1/n)**n
+      sleep(0.25)
+      print(n, ":  ", e)
+      n += exponent
+    print("Calculation of e as a limit completed.")
